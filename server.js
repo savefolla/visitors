5ce466350e8458889a37829a17cd568b185bc6cc
Saverio Follador <saverio@moku.io>
2019-05-25 16:29:31 +0200

    date filename

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-const upload = multer({dest: 'public/images/'});
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, '' + Date.now())
+  }
+});
+const upload = multer({storage});
 
 app.use(express.static('public'));
 
