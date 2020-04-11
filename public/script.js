3ee5954faf4563777af13ac1b7ebf5df80fc4a6f
Saverio Follador <saverio@moku.io>
2020-04-11 16:17:51 +0200

    prettier script

--- public/script.js
+++ public/script.js
@@ -1,54 +1,56 @@
 const onWindowResize = () => {
   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 };
 
 onWindowResize();
 
-window.addEventListener('resize', onWindowResize);
+window.addEventListener("resize", onWindowResize);
 
-const player = document.getElementById('player');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const captureButton = document.getElementById('capture');
-const uploadYoursButton = document.getElementById('upload-yours');
-const image = document.getElementById('random-image');
-const messageWrapper = document.querySelector('.message-wrapper');
-const homeMessage = document.querySelector('.message--home');
-const thanksMessage = document.querySelector('.message--thanks');
-const yourImage = document.getElementById('your-image');
-const count = document.getElementById('count');
+const player = document.getElementById("player");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const captureButton = document.getElementById("capture");
+const uploadYoursButton = document.getElementById("upload-yours");
+const image = document.getElementById("random-image");
+const messageWrapper = document.querySelector(".message-wrapper");
+const homeMessage = document.querySelector(".message--home");
+const thanksMessage = document.querySelector(".message--thanks");
+const yourImage = document.getElementById("your-image");
+const count = document.getElementById("count");
 
 let total;
 let cameraEnabled = false;
 
-uploadYoursButton.addEventListener('click', (e) => {
+uploadYoursButton.addEventListener("click", (e) => {
   e.stopPropagation();
-  image.style.display = 'none';
-  yourImage.style.display = 'block';
-  navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: false,
-    facingMode: 'user'
-  }).then((stream) => {
-    cameraEnabled = true;
-    player.srcObject = stream;
-  });
+  image.style.display = "none";
+  yourImage.style.display = "block";
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+      facingMode: "user",
+    })
+    .then((stream) => {
+      cameraEnabled = true;
+      player.srcObject = stream;
+    });
 });
 
-captureButton.addEventListener('click', () => {
+captureButton.addEventListener("click", () => {
   if (!cameraEnabled) return;
   canvas.width = player.videoWidth;
   canvas.height = player.videoHeight;
   context.drawImage(player, 0, 0, player.videoWidth, player.videoHeight);
-  canvas.toBlob(blob => {
+  canvas.toBlob((blob) => {
     image.src = URL.createObjectURL(blob);
-    image.style.display = 'none';
-    messageWrapper.style.animation = 'none';
-    thanksMessage.style.display = 'block';
-    homeMessage.style.display = 'none';
-    yourImage.style.display = 'none';
-    image.style.display = 'block';
+    image.style.display = "none";
+    messageWrapper.style.animation = "none";
+    thanksMessage.style.display = "block";
+    homeMessage.style.display = "none";
+    yourImage.style.display = "none";
+    image.style.display = "block";
     delete messageWrapper.style.animation;
     setCount(++total);
     postImage(blob);
@@ -73,10 +75,10 @@ getImage = () => {
     const res = JSON.parse(this.responseText);
     total = res.total;
     setCount(res.total);
-    image.src = 'images/' + res.url;
-    image.style.display = 'block';
-    homeMessage.style.display = 'block';
-    yourImage.style.display = 'none';
+    image.src = "images/" + res.url;
+    image.style.display = "block";
+    homeMessage.style.display = "block";
+    yourImage.style.display = "none";
   }
 
   var xhr = new XMLHttpRequest();
@@ -85,6 +87,6 @@ getImage = () => {
   xhr.send();
 };
 
-image.addEventListener('click', getImage);
+image.addEventListener("click", getImage);
 
 getImage();
--- server.js
+++ server.js
@@ -1,66 +1,68 @@
 const port = process.env.PORT || 3000;
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const path = require('path');
-const helmet = require('helmet');
-const compression = require('compression');
-const multer = require('multer');
+const fs = require("fs");
+const path = require("path");
+const helmet = require("helmet");
+const compression = require("compression");
+const multer = require("multer");
 
 app.use(compression());
 app.use(helmet());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/images/')
+    cb(null, "public/images/");
   },
   filename: function (req, file, cb) {
-    cb(null, '' + Date.now())
-  }
+    cb(null, "" + Date.now());
+  },
 });
 const upload = multer({
   storage,
   limits: {
     fileSize: 5 * 1024 * 1024,
-    files: 1
+    files: 1,
   },
   fileFilter: function (req, file, callback) {
-    if (!file.mimetype.includes('image')) {
-      return callback(new Error('Only images allowed!'));
+    if (!file.mimetype.includes("image")) {
+      return callback(new Error("Only images allowed!"));
     }
-    callback(null, true)
+    callback(null, true);
   },
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.get('/image', (req, res) => {
-  const directoryPath = path.join(__dirname, 'public', 'images');
+app.get("/image", (req, res) => {
+  const directoryPath = path.join(__dirname, "public", "images");
   fs.readdir(directoryPath, function (err, files) {
-    if (files) res.send({
-      url: files[Math.floor(Math.random() * files.length)],
-      total: files.length
-    });
-    else res.send({
-      url: '',
-      total: 0
-    });
+    if (files)
+      res.send({
+        url: files[Math.floor(Math.random() * files.length)],
+        total: files.length,
+      });
+    else
+      res.send({
+        url: "",
+        total: 0,
+      });
   });
 });
 
-app.get('/pleaseshoemethephoto/:index', (req, res) => {
-  const directoryPath = path.join(__dirname, 'public', 'images');
+app.get("/pleaseshoemethephoto/:index", (req, res) => {
+  const directoryPath = path.join(__dirname, "public", "images");
   fs.readdir(directoryPath, function (err, files) {
-    res.send(`<img src=${path.join('/images/' + files[req.params.index])} />`);
+    res.send(`<img src=${path.join("/images/" + files[req.params.index])} />`);
   });
 });
 
-app.post('/image', upload.single('image'), (req, res) => {
+app.post("/image", upload.single("image"), (req, res) => {
   res.sendStatus(200);
 });
 
